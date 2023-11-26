6270f7e6211584c3d5d5fa0124c99d25f99226b3
kenz0id <99891662+kenz0id@users.noreply.github.com>
2023-11-26 21:43:25 +0800

    dsfdsfe

--- export/release/windows/haxe/ApplicationMain.hx
+++ export/release/windows/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "13";
+		app.meta["build"] = "2";
 		app.meta["company"] = "ShadowMario";
 		app.meta["file"] = "PsychEngine";
 		app.meta["name"] = "Friday Night Funkin': Psych Engine";
